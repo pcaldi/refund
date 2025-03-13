6bc2e4c5dd4017161ca80568721bf00cc0c622de
pcaldi <pcaldi@gmail.com>
2025-03-13 16:48:32 -0300

    instalando e configurando react-router, criando auth-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { Routes } from "./routes";
+
+
 export function App() {
-    return <h1 className="text-amber-800 text-5xl">App</h1>
+    return <Routes />
 }
---
+++ src/routes/auth-routes.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router";
+
+
+import { SignIn } from "../pages/SignIn";
+
+export function AuthRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<SignIn />} />
+        </Routes>
+    )
+}
