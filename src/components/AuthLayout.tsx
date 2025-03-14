1a779718955e4f05316f4285fe8806f0049ceb5d
pcaldi <pcaldi@gmail.com>
2025-03-14 11:40:32 -0300

    component de Input

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -7,7 +7,7 @@ export function AuthLayout() {
     return (
         <div className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center text-gray-100">
             <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col md:min-w-[462px]">
-                <img className="my-8" src={logoSVG} alt="Logo da aplicação" />
+                <img src={logoSVG} alt="Logo da aplicação" className="my-8" />
                 <Outlet />
             </main>
 
---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+type Props = React.ComponentProps<"input"> & {
+    legend?: string
+}
+
+export function Input({ legend, ...rest }: Props) {
+    return (
+        <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
+
+            {legend &&
+                (<legend className="uppercase text-xxs mb-2 text-inherit">
+                    {legend}
+                </legend>)}
+
+
+            <input type="text" className="w-full h-12 rounded-lg text-gray-100 border border-gray-300 bg-transparent px-4 text-sm placeholder:text-gray-300 outline-none focus:border-2 focus:border-green-100" {...rest} />
+        </fieldset>
+
+    )
+}
