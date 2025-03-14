25166fb921a9a43cebadea0289361d0cca6871bc
pcaldi <pcaldi@gmail.com>
2025-03-14 17:35:36 -0300

    criado page notFound

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
+import { NotFound } from "../pages/NotFound";
 
 import { AuthLayout } from "../components/AuthLayout";
 
@@ -13,6 +14,8 @@ export function AuthRoutes() {
                 <Route path="/" element={<SignIn />} />
                 <Route path="/signUp" element={<SignUp />} />
             </Route>
+
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
