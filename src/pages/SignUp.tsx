c31a78d98e8623a2a0ea4d0c435abf1eaba226a5
pcaldi <pcaldi@gmail.com>
2025-03-14 17:25:41 -0300

    criado page signUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,20 +3,29 @@ import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
-export function SignIn() {
+export function SignUp() {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [passwordConfirm, setPasswordConfirm] = useState("")
     const [isLoading, setIsLoading] = useState(false)
 
     function onSubmit(e: React.FormEvent) {
         e.preventDefault()
 
-        console.log(email, password)
+        console.log(name, email, password, passwordConfirm)
     }
 
     return (
         <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+            <Input
+                required
+                legend="Nome"
+                placeholder="Seu nome"
+                onChange={(e) => setName(e.target.value)}
+            />
+
             <Input
                 required
                 legend="E-mail"
@@ -33,15 +42,23 @@ export function SignIn() {
                 onChange={(e) => setPassword(e.target.value)}
             />
 
+            <Input
+                required
+                legend="Confirmar senha"
+                type="password"
+                placeholder="Confirme sua senha"
+                onChange={(e) => setPasswordConfirm(e.target.value)}
+            />
+
             <Button type="submit" isLoading={isLoading}>
-                Entrar
+                Cadastrar
             </Button>
 
             <a
-                href="/signup"
+                href="/"
                 className="text-sm font-semibold text-gray-100 text-center mt-10 mb-4 hover:text-green-800 transition ease-linear"
             >
-                Criar conta
+                Já tenho uma conta
             </a>
         </form>
     )
--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 
 
 import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
+
 import { AuthLayout } from "../components/AuthLayout";
 
 export function AuthRoutes() {
@@ -9,6 +11,7 @@ export function AuthRoutes() {
         <Routes>
             <Route path="/" element={<AuthLayout />}>
                 <Route path="/" element={<SignIn />} />
+                <Route path="/signUp" element={<SignUp />} />
             </Route>
         </Routes>
     )
