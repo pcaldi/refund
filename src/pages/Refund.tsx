284ec541b74d2b4db2f49f8a9a4ae6091f7ce0e4
pcaldi <pcaldi@gmail.com>
2025-03-18 14:58:46 -0300

    criando header

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,10 +1,9 @@
 
-import { Header } from "../components/Header"
 
 export function Refund() {
     return (
         <div>
-            <Header />
+
             <h1> Refund</h1>
 
         </div >
