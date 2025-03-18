0dba0ef0a110abaf6f55ac88ce0c15215449a886
pcaldi <pcaldi@gmail.com>
2025-03-18 16:05:27 -0300

    criando as categorias

---
+++ src/utils/categories.ts
@@ -0,0 +1,32 @@
+import foodSvg from "../assets/food.svg"
+import othersSvg from "../assets/others.svg"
+import transportSvg from "../assets/transport.svg"
+import accommodationSvg from "../assets/accommodation.svg"
+import servicesSvg from "../assets/services.svg"
+
+
+export const CATEGORIES = {
+    FOOD: {
+        icon: foodSvg,
+        name: "Comida",
+    },
+    OTHERS: {
+        icon: othersSvg,
+        name: "Outros",
+    },
+    TRANSPORT: {
+        icon: transportSvg,
+        name: "Transporte",
+    },
+    ACCOMMODATION: {
+        icon: accommodationSvg,
+        name: "Hospedagem",
+    },
+    SERVICES: {
+        icon: servicesSvg,
+        name: "Serviços",
+    },
+}
+
+
+export const CATEGORIES_KEY = Object.keys(CATEGORIES) as Array<keyof typeof CATEGORIES>
