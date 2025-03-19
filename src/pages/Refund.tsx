7f26318c4b576cef31902e31a8370420a1a9c6a0
pcaldi <pcaldi@gmail.com>
2025-03-19 10:51:55 -0300

    adicionado o button de enviar e estados dos campos com a function onSubmit

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -5,15 +5,32 @@ import { CATEGORIES, CATEGORIES_KEY } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
 
 
 export function Refund() {
 
+    const [name, setName] = useState("")
+    const [amount, setAmount] = useState("")
     const [category, setCategory] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [filename, setFilename] = useState<File | null>(null)
+
+    function onSubmit(e: React.FormEvent) {
+        e.preventDefault()
+
+        console.log(name, category, amount, filename)
+        setName("")
+        setCategory("")
+        setAmount("")
+        setFilename(null)
+
+
+    }
 
 
     return (
-        <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
+        <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
             <header>
                 <h1 className="font-bold text-xl text-gray-100">Solicitação de reembolso</h1>
                 <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso.</p>
@@ -22,6 +39,8 @@ export function Refund() {
             <Input
                 required
                 legend="Nome da solicitação"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
             />
 
             <div className="flex gap-4">
@@ -44,12 +63,20 @@ export function Refund() {
                     required
                     legend="Valor"
                     placeholder="0,00"
+                    onChange={(e) => setAmount(e.target.value)}
+                    value={amount}
                 />
             </div>
 
             <Upload
                 legend="Comprovante"
+                filename={filename && filename.name}
+                onChange={(e) => e.target.files && setFilename(e.target.files[0])}
             />
+
+            <Button type="submit" isLoading={isLoading}>
+                Enviar
+            </Button>
 
         </form >
 
