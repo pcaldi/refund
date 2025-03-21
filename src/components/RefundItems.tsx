5cff0a3af0b60b6b292936cae5789e79fee61ccb
pcaldi <pcaldi@gmail.com>
2025-03-21 13:19:36 -0300

    customizando scroll bar

---
+++ src/components/RefundItems.tsx
@@ -0,0 +1,39 @@
+
+type Props = React.ComponentProps<"a"> & {
+    data: RefundItemProps
+}
+
+export type RefundItemProps = {
+    id: string
+    name: string
+    amount: string
+    category: string
+    categoryImg: string
+}
+
+
+export function RefundItem({ data, ...rest }: Props) {
+    return (
+        <a
+            className="flex flex-1 hover:bg-green-100/5 p-2 rounded-md items-center cursor-pointer gap-4"
+            {...rest}
+        >
+
+            <img
+                className="w-8 h-8"
+                src={data.categoryImg}
+                alt="Ícone de category"
+            />
+
+            <div className="flex flex-col flex-1">
+                <strong className="text-sm text-gray-100">{data.name}</strong>
+                <span className="text-xs text-gray-200">{data.category}</span>
+            </div>
+
+            <span className="text-sm text-gray-100 font-semibold">
+                <small className="font-base text-gray-200">R$ </small>
+                {data.amount}
+            </span>
+        </a>
+    )
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,8 +2,21 @@ import { useState } from "react";
 
 import searchSvg from "../assets/search.svg"
 
+
+import { CATEGORIES } from "../utils/categories";
+
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { RefundItem } from "../components/RefundItems";
+
+
+const REFUND_ITEMS_EXAMPLE = {
+    id: "123",
+    name: "Paulo",
+    amount: "72,5",
+    category: "Transporte",
+    categoryImg: CATEGORIES.TRANSPORT.icon,
+}
 
 export function Dashboard() {
     const [search, setSearch] = useState("")
@@ -13,19 +26,34 @@ export function Dashboard() {
         e.preventDefault()
 
         console.log(search)
-    }
 
+    }
 
     return (
         <div className="bg-gray-500 rounded-xl p-10 w-full md:min-w-[762px]">
             <h1 className="font-bold text-xl text-gray-100 flex-1">Solicitações</h1>
 
             <form onSubmit={fetchRefunds} className="flex flex-1 gap-2 mt-6 border-b-[1px]  border-gray-400 pb-6 md:flex-row">
-                <Input placeholder="Pesquisar pelo nome" onChange={(e) => setSearch(e.target.value)} value={search} />
+
+                <Input
+                    placeholder="Pesquisar pelo nome"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                />
+
                 <Button type="submit" variant="icon">
-                    <img src={searchSvg} alt="Ícone de uma lupa" />
+                    <img
+                        src={searchSvg}
+                        alt="Ícone de uma lupa"
+                        className="w-5"
+                    />
                 </Button>
+
             </form>
+
+            <div className="mt-6 flex flex-col gap-4 max-h-[345px] overflow-y-scroll">
+                <RefundItem data={REFUND_ITEMS_EXAMPLE} />
+            </div>
 
 
 
