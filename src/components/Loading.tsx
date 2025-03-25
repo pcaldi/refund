54a6ecfffbb4bf6266aa6a7313cadde980f718b7
pcaldi <pcaldi@gmail.com>
2025-03-25 17:33:23 -0300

    component loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+    return (
+        <div className="flex w-screen h-screen items-center justify-center">
+            <span className="font-semibold text-gray-200 text-sm">Carregando...</span>
+        </div>
+    )
+}
