2f15e48ad7963aecb8d600ec6650aa05b206c60a
pcaldi <pcaldi@gmail.com>
2025-03-25 17:34:04 -0300

    role usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,41 @@
 import { BrowserRouter } from "react-router";
 
+import { Loading } from "../components/Loading";
+
 import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 
+
+const isLoading = false;
+
 export function Routes() {
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    const session = {
+        user: {
+            role: "",
+        }
+    }
+
+    function Route() {
+        switch (session.user.role) {
+            case "manager":
+                return <ManagerRoutes />
+            case "employee":
+                return <EmployeeRoutes />
+            default:
+                return <AuthRoutes />
+        }
+    }
+
+
     return (
         <BrowserRouter>
-            <ManagerRoutes />
+            <Route />
         </BrowserRouter>
     )
 }
