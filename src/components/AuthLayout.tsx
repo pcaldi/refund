be0c677fd8ff804d485b9be3c16554002d52387b
pcaldi <pcaldi@gmail.com>
2025-03-25 17:34:46 -0300

    adicionado md:max-w e padding

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -5,8 +5,8 @@ import logoSVG from "../assets/logo.svg"
 
 export function AuthLayout() {
     return (
-        <div className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center text-gray-100">
-            <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col md:min-w-[462px]">
+        <div className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center text-gray-100 p-8">
+            <main className="bg-gray-500 rounded-md flex items-center flex-col w-full md:max-w-[462px] p-8">
                 <img src={logoSVG} alt="Logo da aplicação" className="my-8" />
                 <Outlet />
             </main>
