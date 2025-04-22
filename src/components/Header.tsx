ccea0de2d0438da8c22a1d1b47e972991e08c5d8
pcaldi <pcaldi@gmail.com>
2025-04-22 16:50:45 -0300

    add session user name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import logoSvg from "../assets/logo.svg"
 import logoutSvg from "../assets/logout.svg"
 
+import { useAuth } from "../hooks/useAuth"
+
 export function Header() {
+    const { session } = useAuth()
+
+
     return (
         <header className="w-full flex justify-between">
             <img src={logoSvg} alt="logo" className="my-8" />
 
             <div className="flex items-center gap-3">
-                <span className="flex text-sm font-semibold text-gray-200 ">Olá, Paulo</span>
+                <span className="flex text-sm font-semibold text-gray-200 ">{`Olá, ${session?.user.name}`}</span>
                 <img src={logoutSvg} alt="logout" className="my-8 cursor-pointer hover:opacity-75 transition ease-linear" />
             </div>
         </header>
